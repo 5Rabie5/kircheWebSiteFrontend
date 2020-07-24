6d9d02c6f978322d1c33b1fac206718c7eba84b0
5Rabie5 <n.rabietarboush@gmail.com>
2020-07-24 18:31:45 +0200

    add  routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./component/home/home.component";
+import {HeaderComponent} from "./component/header/header.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'header', component: HeaderComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
